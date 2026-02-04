2cdb1fcd99560b470128825afe3ded06880566cb
Jay Fatiya <jayantilal.fatiya@bti.bmwgroup.com>
2026-02-04 12:33:28 +0530

    modified functions

---
+++ domain/src/main/java/com/jay/domain/util/decimalPattern.kt
@@ -0,0 +1,12 @@
+package com.jay.domain.util
+
+fun String.decimalPatternCustom(): String {
+    val decimalCount = this.length - 1 - this.indexOf(".")
+    val decimalPattern = StringBuilder()
+    var i = 0
+    while (i < decimalCount && i < 2) {
+        decimalPattern.append("*")
+        i++
+    }
+    return decimalPattern.toString()
+}
